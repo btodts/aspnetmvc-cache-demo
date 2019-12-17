dc4e9fbc6dab07b72a5c5e49a2d3f699a30a1444
Benjamin Todts <Benjamin.Todts@mediahuis.be>
2019-12-17 16:28:47 +0100

    Add project files.

---
+++ MvcTest/CustomCacheAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MvcTest
+{
+    public class CustomCacheAttribute : ActionFilterAttribute
+    {
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            filterContext.HttpContext.Response.Cache.SetMaxAge(new System.TimeSpan(5, 5, 5));
+            filterContext.HttpContext.Response.Cache.SetCacheability(System.Web.HttpCacheability.Public);
+        }
+    }
+}
